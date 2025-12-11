2dcdf7e0d2d9030d53ec7ea6a242fea7c60e5d1b
Ella Schwarz <schwarzem@a6.Millennium.Berkeley.EDU>
2025-12-11 14:01:58 -0800

    add run-binary-fast-debug option

--- common.mk
+++ common.mk
@@ -324,7 +324,7 @@ verilog: $(sim_common_files)
 #########################################################################################
 # helper rules to run simulations
 #########################################################################################
-.PHONY: run-binary run-binary-fast run-binary-debug run-fast
+.PHONY: run-binary run-binary-fast run-binary-debug run-binary-fast-debug run-fast
 	%.check-exists check-binary check-binaries
 
 check-binary:
@@ -382,12 +382,27 @@ run-binaries: check-binaries $(addsuffix .run,$(wildcard $(BINARIES)))
 
 # run simulator as fast as possible (no insn disassembly)
 run-binary-fast: check-binary $(BINARY).run.fast
+run-binary-fast-debug: check-binary $(BINARY).run.fastdebug
 run-binaries-fast: check-binaries $(addsuffix .run.fast,$(wildcard $(BINARIES)))
 
 %.run.fast: %.check-exists $(SIM_PREREQ) | $(output_dir)
 	(set -o pipefail && $(NUMA_PREFIX) $(sim) \
 		$(PERMISSIVE_ON) \
 		$(call get_common_sim_flags,$*) \
+		$(PERMISSIVE_OFF) \
+		$* \
+		$(BINARY_ARGS) \
+		</dev/null | tee $(call get_sim_out_name,$*).log)
+
+%.run.fastdebug: %.check-exists $(SIM_DEBUG_PREREQ) | $(output_dir)
+ifeq (1,$(DUMP_BINARY))
+	if [ "$*" != "none" ]; then riscv64-unknown-elf-objdump -D -S $* > $(call get_sim_out_name,$*).dump ; fi
+endif
+	(set -o pipefail && $(NUMA_PREFIX) $(sim_debug) \
+		$(PERMISSIVE_ON) \
+		$(call get_common_sim_flags,$*) \
+		$(VERBOSE_FLAGS) \
+		$(call get_waveform_flag,$(call get_sim_out_name,$*)) \
 		$(PERMISSIVE_OFF) \
 		$* \
 		$(BINARY_ARGS) \
